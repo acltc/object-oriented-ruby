10eb7a868fcb59c843a82a916b3fa71f3ff6c1dc
Peter Jang <peter.x.jang@gmail.com>
2016-02-11 16:11:17 -0600

    separated classes and modules into separate files

--- employee.rb
+++ employee.rb
@@ -1,24 +1,6 @@
-# 1. Represent an employee as an array
-employee1 = ["Majora", "Carter", 80000, true]
-employee2 = ["Danilo", "Campos", 70000, true]
-puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-
-# 2. Represent an employee as a hash
-employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
-employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
-puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
-puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
-
-# 3. Represent an employee as a hash with symbols
-employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
-employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
-puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-
-# 4. Represent an employee as a class instance
 class Employee
   attr_reader :first_name, :last_name
+  attr_accessor :active
 
   def initialize(input_options)
     @first_name = input_options[:first_name]
@@ -35,12 +17,3 @@ class Employee
     @salary = 1.05 * @salary
   end
 end
-
-employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
-puts employee2.first_name
-puts employee2.last_name
