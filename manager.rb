56ace969f0a7c0b1322c51fb7e2d1a86f80f645d
Peter Jang <peter.x.jang@gmail.com>
2016-02-11 13:10:49 -0600

    used inheritance to DRY up Manager class

--- manager.rb
+++ manager.rb
@@ -17,24 +17,7 @@ class Employee
   end
 end
 
-class Manager
-  attr_reader :first_name, :last_name
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @salary = input_options[:salary]
-    @active = input_options[:active]
-  end
-
-  def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
-  end
-
-  def give_annual_raise
-    @salary = 1.05 * @salary
-  end
-
+class Manager < Employee
   def send_report
     puts "Sending email..."
     # use email sending library...
