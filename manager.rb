d55c778db7a66218930f060998349222e4ed825c
Peter Jang <peter.x.jang@gmail.com>
2016-02-11 14:01:03 -0600

    added Manager method to give all employees raises

--- manager.rb
+++ manager.rb
@@ -28,6 +28,12 @@ class Manager < Employee
     # use email sending library...
     puts "Email sent!"
   end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
 end
 
 employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
@@ -38,3 +44,5 @@ employee2.print_info
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
+manager.give_all_raises
+employee1.print_info
