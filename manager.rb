2105b796f577bd0e190fcd775b687b855b05bd96
Peter Jang <peter.x.jang@gmail.com>
2016-02-11 16:59:04 -0600

    wrapped classes and modules in ACLTC namespace module

--- manager.rb
+++ manager.rb
@@ -1,23 +1,25 @@
 require './employee.rb'
 require './email_reporter.rb'
 
-class Manager < Employee
-  include EmailReporter
+module ACLTC
+  class Manager < Employee
+    include EmailReporter
 
-  def initialize(input_options)
-    super(input_options)
-    @employees = input_options[:employees]
-  end
-
-  def give_all_raises
-    @employees.each do |employee|
-      employee.give_annual_raise
+    def initialize(input_options)
+      super(input_options)
+      @employees = input_options[:employees]
     end
-  end
 
-  def fire_all_employees
-    @employees.each do |employee|
-      employee.active = false
+    def give_all_raises
+      @employees.each do |employee|
+        employee.give_annual_raise
+      end
+    end
+
+    def fire_all_employees
+      @employees.each do |employee|
+        employee.active = false
+      end
     end
   end
 end
