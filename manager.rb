f92e232142dc3b8b57597565a651636bd4f6d0a7
Peter Jang <peter.x.jang@gmail.com>
2016-02-11 13:41:18 -0600

    added custom initialize method to Manager class (non-DRY)

--- manager.rb
+++ manager.rb
@@ -18,6 +18,14 @@ class Employee
 end
 
 class Manager < Employee
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+    @employees = input_options[:employees]
+  end
+
   def send_report
     puts "Sending email..."
     # use email sending library...
@@ -30,6 +38,6 @@ employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 7000
 employee1.print_info
 employee2.print_info
 
-manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true)
+manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
